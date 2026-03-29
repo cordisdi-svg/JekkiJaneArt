178cc16907c8914f698cd41fa1b808cee015260a
cordisdi-svg <cordllsdll@gmail.com>
2026-03-30 00:35:21 +0700

    feat: initialize root layout component for applic

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,8 +7,23 @@ import { DeviceLayoutSync } from "@/components/system/DeviceLayoutSync";
 import { InstagramTooltip } from "@/components/ui/InstagramTooltip";
 
 export const metadata: Metadata = {
-  title: "Портфолио художницы",
-  description: "Описание сайта будет добавлено позже"
+  title: "JekkiJaneArt — искусство в уникальном стиле, делающее вашу жизнь, дом и образ ярче и эстетичнее",
+  description: "Арт-галерея JekkiJaneArt где вы можете заказать воплощение любой своей художественной идеи на холсте, стене, одежде; выбрать из уже написанных картин или создать картину-проводник для своей энергии",
+  openGraph: {
+    type: "website",
+    title: "JekkiJaneArt — искусство в уникальном стиле, делающее вашу жизнь, дом и образ ярче и эстетичнее",
+    description: "Арт-галерея JekkiJaneArt где вы можете заказать воплощение любой своей художественной идеи на холсте, стене, одежде; выбрать из уже написанных картин или создать картину-проводник для своей энергии",
+    url: "https://jekkiart.ru",
+    images: [{
+      url: "https://raw.githubusercontent.com/cordisdi-svg/JekkiJaneArt/f2ac6d0bc33c6d548209d35c6f245277c1b46f32/public/og-image.jpg"
+    }]
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "JekkiJaneArt — искусство в уникальном стиле, делающее вашу жизнь, дом и образ ярче и эстетичнее",
+    description: "Арт-галерея JekkiJaneArt где вы можете заказать воплощение любой своей художественной идеи на холсте, стене, одежде; выбрать из уже написанных картин или создать картину-проводник для своей энергии",
+    images: ["https://raw.githubusercontent.com/cordisdi-svg/JekkiJaneArt/f2ac6d0bc33c6d548209d35c6f245277c1b46f32/public/og-image.jpg"]
+  }
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
